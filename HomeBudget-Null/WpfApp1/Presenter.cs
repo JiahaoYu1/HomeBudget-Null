7b000a56c49dbe125e927de88dd5cd648c44c508
Caden Marion <2168153@johnabbottcollege.net>
2023-04-04 13:32:42 -0400

    Added SaveToFile()

--- HomeBudget-Null/WpfApp1/Presenter.cs
+++ HomeBudget-Null/WpfApp1/Presenter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Budget;
@@ -11,11 +12,13 @@ namespace WpfApp1
     {
         private ViewInterface view;
         private HomeBudget budget;
+        private string dbFileName;
 
         public Presenter(string dbFile, ViewInterface newView)
         {
             view = newView;
-            budget = new HomeBudget(dbFile);
+            dbFileName = dbFile;
+            budget = new HomeBudget(dbFileName);
         }
 
         public void AddExpense(DateTime date, int categoryId, double amount, string desc)
@@ -26,6 +29,11 @@ namespace WpfApp1
         public void AddCategory(string name, Budget.Category.CategoryType type = Budget.Category.CategoryType.Expense)
         {
             budget.categories.Add(name, type);
+        }
+
+        public void SaveToFile()
+        {
+            budget.SaveToFile(dbFileName);
         }
 
         //public double GetBudget()
