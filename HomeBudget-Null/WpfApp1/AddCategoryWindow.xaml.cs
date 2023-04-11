b2ee2bd58b8af3677a3b0d1177b21504068e5f26
JiahaoYu1 <98350594+JiahaoYu1@users.noreply.github.com>
2023-04-11 12:40:05 -0400

    New Window when add Category button clicked

---
+++ HomeBudget-Null/WpfApp1/AddCategoryWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for AddCategoryWindow.xaml
+    /// </summary>
+    public partial class AddCategoryWindow : Window
+    {
+        public string CategoryName { get; set; }
+        public string CategoryType { get; set; }
+        public AddCategoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void confirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(categoryNameTextBox.Text))
+            {
+                MessageBox.Show("Category name cannot be empty.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            CategoryName = categoryNameTextBox.Text;
+            CategoryType = categoryTypeComboBox.Text;
+            DialogResult = true;
+        }
+    }
+}
