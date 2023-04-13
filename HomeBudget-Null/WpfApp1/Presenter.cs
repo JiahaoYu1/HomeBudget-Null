ffc54df0ede79332c8ab15824be4ae421ca78bd4
Caden Marion <2168153@johnabbottcollege.net>
2023-04-13 12:30:14 -0400

    Added documentation (to make life a bit easier)

--- HomeBudget-Null/WpfApp1/Presenter.cs
+++ HomeBudget-Null/WpfApp1/Presenter.cs
@@ -14,6 +14,11 @@ namespace WpfApp1
         private HomeBudget budget;
         private string dbFileName;
 
+        /// <summary>
+        /// Initializes a new instance of the Presenter class
+        /// </summary>
+        /// <param name="dbFile">The database file of the budget to use</param>
+        /// <param name="newView">The view to use for displaying</param>
         public Presenter(string dbFile, ViewInterface newView)
         {
             view = newView;
@@ -21,16 +26,31 @@ namespace WpfApp1
             budget = new HomeBudget(dbFileName);
         }
 
+        /// <summary>
+        /// Adds a new expense to the budget
+        /// </summary>
+        /// <param name="date">The date of the expense</param>
+        /// <param name="categoryId">The ID number of the specific category associated with the expense</param>
+        /// <param name="amount">The amount of money expended or gained</param>
+        /// <param name="desc">The name of the expense</param>
         public void AddExpense(DateTime date, int categoryId, double amount, string desc)
         {
             budget.expenses.Add(date, categoryId, amount, desc);
         }
 
+        /// <summary>
+        /// Adds a new expense category to the budget
+        /// </summary>
+        /// <param name="name">The name of the category</param>
+        /// <param name="type">The type of category (Expense, Savings, Income, Credit)</param>
         public void AddCategory(string name, Budget.Category.CategoryType type = Budget.Category.CategoryType.Expense)
         {
             budget.categories.Add(name, type);
         }
 
+        /// <summary>
+        /// Saves the budget to a database file
+        /// </summary>
         public void SaveToFile()
         {
             budget.SaveToFile(dbFileName);
