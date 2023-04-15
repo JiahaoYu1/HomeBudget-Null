156766bfd632a48bf47deba5a91640625c5cd684
Jiahao Yu <kevinyjh7@gmail.com>
2023-04-15 16:12:35 -0400

    Tried to do the theme button but didnt figure out, DateTime picker set to today added

--- HomeBudget-Null/WpfApp1/MainWindow.xaml.cs
+++ HomeBudget-Null/WpfApp1/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Budget;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Media;
 
 namespace WpfApp1
 {
@@ -19,7 +22,16 @@ namespace WpfApp1
         {
             InitializeComponent();
             this.presenter = new Presenter(dbFile, this);
+            dateDatePicker.SelectedDate = DateTime.Today;
+        }
 
+        // Define color themes
+        public enum ColorTheme
+        {
+            Light,
+            Dark,
+            Blue,
+            Green
         }
 
         public void AddCategory()
@@ -72,15 +84,30 @@ namespace WpfApp1
             }
         }
 
-
         private void addButton_Click(object sender, RoutedEventArgs e)
         {
-            unsavedChanges = false;
-            /* Expense expense = new Expense(nameTextBox.Text, dateDatePicker.Text, categoryComboBox.SelectedItem.ToString(), double.Parse(amountTextBox.Text), descriptionTextBox.Text);
+            /*// Create a new Expense object using the values entered by the user
+            string name = nameTextBox.Text;
+            DateTime date = dateDatePicker.SelectedDate.Value;
+            string category = categoryComboBox.SelectedItem.ToString();
+            double amount = double.Parse(amountTextBox.Text);
+            string description = descriptionTextBox.Text;
+            Expense expense = new Expense(id, date, category, amount, description);
 
-             presenter.AddExpense(expense);
+            // Add the expense to the budget using the presenter
+            presenter.AddExpense(expense);
 
-             budgetLabel.Content = "Budget: $" + presenter.GetBudget().ToString("F2");*/
+            // Update the budget label
+            budgetLabel.Content = "Budget: $" + presenter.GetBudget().ToString("F2");
+
+            // Clear the form
+            nameTextBox.Text = "";
+            amountTextBox.Text = "";
+            dateDatePicker.SelectedDate = null;
+            categoryComboBox.SelectedIndex = -1;*/
+
+            // Set unsavedChanges to true
+            unsavedChanges = true;
         }
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
@@ -90,7 +117,7 @@ namespace WpfApp1
             amountTextBox.Text = "";
             dateDatePicker.Text = "";
             categoryComboBox.SelectedIndex = -1;
-            //descriptionTextBox.Text = "";
+
         }
 
         private void chooseFile_Click(object sender, RoutedEventArgs e)
@@ -141,6 +168,57 @@ namespace WpfApp1
                 if (result == MessageBoxResult.No)
                     e.Cancel = true;
             }
+        }
+
+        private void descriptionTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            unsavedChanges = true;
+        }
+
+        private void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+           /* // Check if the IsDarkMode key exists in the resources dictionary
+            if (Application.Current.Resources.Contains("IsDarkMode"))
+            {
+                // Check the current theme mode
+                bool isDarkMode = false;
+                if (Application.Current.Resources["IsDarkMode"] is bool value)
+                {
+                    isDarkMode = value;
+                }
+
+                // Toggle the theme mode
+                isDarkMode = !isDarkMode;
+
+                // Set the new theme mode
+                Application.Current.Resources["IsDarkMode"] = isDarkMode;
+
+                // Force a refresh of the UI
+                var window = Application.Current.MainWindow;
+                window.InvalidateMeasure();
+                window.InvalidateArrange();
+                window.UpdateLayout();
+
+                // Set the UI element colors and styles based on the theme mode
+                if (isDarkMode)
+                {
+                    Application.Current.Resources["BackgroundColor"] = Brushes.Black;
+                    Application.Current.Resources["ForegroundColor"] = Brushes.White;
+                    Application.Current.Resources["GroupBoxBackground"] = Brushes.DarkGray;
+                    Application.Current.Resources["GroupBoxForeground"] = Brushes.White;
+                    Application.Current.Resources["ButtonBackground"] = Brushes.DarkGray;
+                    Application.Current.Resources["ButtonForeground"] = Brushes.White;
+                }
+                else
+                {
+                    Application.Current.Resources["BackgroundColor"] = Brushes.White;
+                    Application.Current.Resources["ForegroundColor"] = Brushes.Black;
+                    Application.Current.Resources["GroupBoxBackground"] = Brushes.LightGray;
+                    Application.Current.Resources["GroupBoxForeground"] = Brushes.Black;
+                    Application.Current.Resources["ButtonBackground"] = Brushes.LightGray;
+                    Application.Current.Resources["ButtonForeground"] = Brushes.Black;
+                }
+            }*/
         }
     }
 }
