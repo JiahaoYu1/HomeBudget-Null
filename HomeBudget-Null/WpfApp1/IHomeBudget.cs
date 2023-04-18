3c1aa1b7fcee258bfbd9b3f888ec6bee293eb4a4
Caden Marion <2168153@johnabbottcollege.net>
2023-04-18 13:57:09 -0400

    Initial commit

---
+++ HomeBudget-Null/WpfApp1/IHomeBudget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public interface IHomeBudget
+    {
+        public void DisplayError(Exception e);
+
+        public void GetFile(bool isCreatingNewFile);
+    }
+}
